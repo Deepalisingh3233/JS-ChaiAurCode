a2b71c6816e21f580f3cd7737211575551ea3a19
Deepalisingh3233 <87854909+Deepalisingh3233@users.noreply.github.com>
2025-02-13 11:08:30 +0000

    about heap and stack memory

--- 01_basics/dataTypesSummary.js
+++ 01_basics/dataTypesSummary.js
@@ -12,13 +12,13 @@ let userEmail;
 const Id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(Id);
-console.log(anotherId);
+// console.log(Id);
+// console.log(anotherId);
 
 console.log(Id === anotherId);
 
 const bigNumber = 2342354236458776476n
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
  
 
 
@@ -32,10 +32,33 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("Hello World");   
+    // console.log("Hello World");   
 }
 
-console.log(typeof myFunction) //? function
-console.log(typeof myObj) //? object (object function)
-console.log(typeof heros) //? object
-console.log(typeof Id) //? symbol
+// console.log(typeof myFunction) //? function
+// console.log(typeof myObj) //? object (object function)
+// console.log(typeof heros) //? object
+// console.log(typeof Id) //? symbol
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++=
+
+//? Memory -> Stack (Primitive  ) , Heap (Non-primitive)
+
+let myYoutubeName = "Deepali Sigh"
+
+let anotherName = myYoutubeName
+anotherName = "chaiaurcode"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne;
+userTwo.email = "usertwo@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
