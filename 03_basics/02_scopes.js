55b95c828ef0e6293aeb19c332908da427359bf7
Deepalisingh3233 <87854909+Deepalisingh3233@users.noreply.github.com>
2025-02-16 09:21:34 +0000

    Scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,9 +3,50 @@ if (true) {
   let a = 10;
   const b = 20;
   //   var c = 30;
-  console.log("Inner - > ", a);
+//   console.log("Inner - > ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "Deepali"
+
+    function two(){
+        const website = "Youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+if(true){
+    const username = "Ritesh"
+    if(username === "Ritesh"){
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);  
+}
+// console.log(username);
+
+
+// +++++++++++++++++intersting++++++++++++++++++
+
+console.log(addOne(5));
+
+function addOne(num){
+    return num + 1
+}
+// console.log(addOne(5));
+
+console.log(addTwo(5));
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
+
